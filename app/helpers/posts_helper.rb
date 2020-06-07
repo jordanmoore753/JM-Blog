6b43dd20f880a317f35d34815da12864c8c121d6
jordanmoore753 <jordanmoore753@gmail.com>
2020-06-07 13:32:52 -0400

    add partial for comments

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def valid_user?(post)
     post.user_id == cookies.encrypted[:user_id] && request.env['PATH_INFO'] == '/profile'
   end
+
+  def valid_user_comment?(comment)
+    comment.user_id == cookies.encrypted[:user_id]
+  end
 end
